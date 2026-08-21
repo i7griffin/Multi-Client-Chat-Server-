a16599ee8213fd1368e132865d86cbcb28b448f2
GRIFFIN <mariagriffinraj.i@gmail.com>
2026-08-21 17:23:03 +0530

    Implement bind() with error handling

--- server.cpp
+++ server.cpp
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <cerrno>
 #include <cstring>
+// header for socaddr_in and htons
+#include <netinet/in.h>
 
 using namespace std;
 
@@ -19,20 +21,36 @@ int main()
     cout << "Socket created successfully!" << endl;
     cout << "File descriptor: " << server_fd << endl;
 
-
-    //this is the structure for server address
+    // this is the structure for server address
     struct sockaddr_in server_addr;
 
     // Zeroing out the memory of the structure
     memset(&server_addr, 0, sizeof(server_addr));
 
+    // to listen through all available local interfaces
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
     // AF_INET is for ipv4 addresses
     server_addr.sin_family = AF_INET;
-    //htons converts the port number to network byte order 
-    server_addr.sin_port = htons(8080); 
+    // htons converts the port number to network byte order
+    server_addr.sin_port = htons(8080);
+
+    // socket function creates the socket and the bind funciton gives the socket a local address
+    // the bind function expects a pointer struck to serveraddr because the bing=d function must eb able to work with all
+    // kinds of address families ,
+    // so the general pointer of sockaddr is expected by the function
+    int result = bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    // each and every thread have unique errno
+    // and the function running in that thread can read or set that threads content
+    if (result == -1)
+    {
+        cout << "Bind failed: " << strerror(errno) << endl;
+        close(server_fd);
+        return 1;
+    }
 
     close(server_fd);
 
     return 0;
 }
-
