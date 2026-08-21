eb80641ba4136a64822d9c79b6dfeeb06bf3a31a
GRIFFIN <mariagriffinraj.i@gmail.com>
2026-08-21 17:01:24 +0530

    added the structure of creating a socket address

--- server.cpp
+++ server.cpp
@@ -19,6 +19,18 @@ int main()
     cout << "Socket created successfully!" << endl;
     cout << "File descriptor: " << server_fd << endl;
 
+
+    //this is the structure for server address
+    struct sockaddr_in server_addr;
+
+    // Zeroing out the memory of the structure
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    // AF_INET is for ipv4 addresses
+    server_addr.sin_family = AF_INET;
+    //htons converts the port number to network byte order 
+    server_addr.sin_port = htons(8080); 
+
     close(server_fd);
 
     return 0;
